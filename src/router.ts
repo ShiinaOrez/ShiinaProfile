f18d38a41f484c098513131ae4b781b6bdb13217
ShiinaOrez <shiina_orez@qq.com>
2020-02-13 23:51:55 +0800

    [update]: docs

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
 
 Vue.use(Router);
 
@@ -14,7 +13,7 @@ export default new Router({
             meta: {
                 title: '',
             },
-            component: Home,
+            component: () => import("./views/Home.vue"),
         },
     ],
 });
